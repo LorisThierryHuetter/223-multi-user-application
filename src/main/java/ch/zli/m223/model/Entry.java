d8542454549fbfb0162bead120e0ba26a51864ab
LorisHütter <huetter@treibauf.ch>
2022-11-22 13:11:08 +0100

    initial commit

---
+++ src/main/java/ch/zli/m223/model/Entry.java
@@ -0,0 +1,68 @@
+package ch.zli.m223.model;
+
+import javax.persistence.*;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+import java.time.LocalDateTime;
+
+@Entity
+public class Entry {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Schema(readOnly = true)
+  private Long id;
+
+  @Column(nullable = false)
+  private LocalDateTime checkIn;
+
+  @Column(nullable = false)
+  private LocalDateTime checkOut;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public LocalDateTime getCheckIn() {
+    return checkIn;
+  }
+
+  public void setCheckIn(LocalDateTime checkIn) {
+    this.checkIn = checkIn;
+  }
+
+  public LocalDateTime getCheckOut() {
+    return checkOut;
+  }
+
+  public void setCheckOut(LocalDateTime checkOut) {
+    this.checkOut = checkOut;
+  }
+}
+
+/**
+
+
+@Column(nullable = false)
+private LocalDateTime checkOut;
+
+
+@ManyToMany
+@JoinTable{
+  name = "entry_tags",
+  joinColumns = @JoinColumn(name = "entry_id"),
+  inverseJoinColumns = @JoinColumn(name = "tag_id")
+}
+@JsonIgnoreProperties("entries")
+@Fetch(FetchMode.JOIN)
+private Set<Tag> tags;
+
+public Long getId(){
+  return id;
+}
+
+ */
