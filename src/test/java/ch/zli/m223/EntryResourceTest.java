540aa284fd97adbe9917cb4838bd5c063ce43df3
LorisHütter <huetter@treibauf.ch>
2022-11-23 13:04:46 +0100

    Updated the test to test application user usages of rest functions

--- src/test/java/ch/zli/m223/EntryResourceTest.java
+++ src/test/java/ch/zli/m223/EntryResourceTest.java
@@ -5,65 +5,88 @@ import io.restassured.http.ContentType;
 
 import org.junit.jupiter.api.Test;
 
-import ch.zli.m223.model.Entry;
+import ch.zli.m223.application_user.ApplicationUser;
 
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
-import java.time.LocalDateTime;
-
 @QuarkusTest
 public class EntryResourceTest {
 
-  @Test
-  public void testIndexEndpoint() {
-    given()
-      .when().get("/entries")
-      .then()
-       .statusCode(200)
-       .body(is("[]"));
-  }
-
-  @Test
-  public void testDeleteEndpoint() {
-    var entry = new Entry();
-    entry.setCheckIn(LocalDateTime.now());
-    entry.setCheckOut(LocalDateTime.now());
-
-    var createResponse = given()
-      .contentType(ContentType.JSON)
-      .body(entry)
-      .when().post("/entries");
-
-    given()
-      .when().delete("/entries/" + createResponse.jsonPath().get("id"))
-      .then()
-        .statusCode(204);
-  }
-
-  @Test
-  public void testUpdateEndpoint() {
-    var entry = new Entry();
-    entry.setCheckIn(LocalDateTime.now());
-    entry.setCheckOut(LocalDateTime.now());
-
-    var createResponse = given()
-      .contentType(ContentType.JSON)
-      .body(entry)
-      .when().post("/entries");
-
-
-    var updatedEntry = new Entry();
-    updatedEntry.setCheckIn(LocalDateTime.of(2023, 05, 1, 1, 1, 1));
-    updatedEntry.setCheckOut(LocalDateTime.of(2023, 05, 1, 1, 1, 2));
-
-    given()
-      .contentType(ContentType.JSON)
-      .body(updatedEntry)
-      .when().put("/entries/" + createResponse.jsonPath().get("id"))
-      .then()
+    @Test
+    public void testIndexEndpoint() {
+        given()
+        .when().get("/entries")
+        .then()
         .statusCode(200)
-        .body("checkIn", is("2023-05-01T01:01:01"));
-  }
+        .body(is("[]"));
+    }
+
+    @Test
+    public void testCreateEndpoint() {
+        
+
+        System.out.println("TEST is empty?" + given().when().get("/entries").then().statusCode(200).body(is("[]")));
+        var applicationuser = new ApplicationUser();
+        applicationuser.setVorname("Joe");
+        applicationuser.setNachname("Biden");
+        applicationuser.setEmail("joe.biden@pres.gov");
+        applicationuser.setPassword("hello");
+
+
+        var createResponse = given()
+            .contentType(ContentType.JSON)
+            .body(applicationuser)
+            .when().post("/entries");
+    }
+
+    @Test
+    public void testDeleteEndpoint() {
+        var applicationuser = new ApplicationUser();
+        applicationuser.setVorname("Joe");
+        applicationuser.setNachname("Biden");
+        applicationuser.setEmail("joe.bidens@husband.gov");
+        applicationuser.setPassword("hello");
+
+        var createResponse = given()
+            .contentType(ContentType.JSON)
+            .body(applicationuser)
+            .when().post("/entries");
+
+        given()
+            .when().delete("/entries/" + createResponse.jsonPath().get("id"))
+            .then()
+            .statusCode(204);
+    }
+
+    @Test
+    public void testUpdateEndpoint() {
+        var applicationuser = new ApplicationUser();
+        applicationuser.setVorname("Joe");
+        applicationuser.setNachname("Biden");
+        applicationuser.setEmail("joe.bidens@husband.gov");
+        applicationuser.setPassword("hello");
+
+        var createResponse = given()
+            .contentType(ContentType.JSON)
+            .body(applicationuser)
+            .when().post("/entries");
+
+
+        var updatedApplicationUser = new ApplicationUser();
+        updatedApplicationUser.setVorname("Boe");
+        updatedApplicationUser.setNachname("Jiden");
+        updatedApplicationUser.setEmail("boe.jidens@husband.gov");
+        updatedApplicationUser.setPassword("hello");
+        // updatedApplicationUser.setId(createResponse.jsonPath().get("id"));
+
+        given()
+            .contentType(ContentType.JSON)
+            .body(updatedApplicationUser)
+            .when().put("/entries/" + createResponse.jsonPath().get("id"))
+            .then()
+            .statusCode(200)
+            .body("email", is("boe.jidens@husband.gov"));
+    }
 
 }
